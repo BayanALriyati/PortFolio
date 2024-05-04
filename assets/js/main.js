d8ccccf6d6cc8600b9ae7814897e06cce6bd0d76
BayanALriyati <bayanalriyati@gmail.com>
2024-05-04 23:29:10 +0300

    cv

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,56 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", "assets/particles.json", function () {
-    console.log("callback - particles.js config loaded");
-  });
+// particlesJS.load("particles-js", "assets/particles.json", function () {
+//     console.log("callback - particles.js config loaded");
+//   });
   
   /* ============= counter number animation ================ */
-  function animate(obj, initVal, lastVal, duration) {
-    let startTime = null;
+  // function animate(obj, initVal, lastVal, duration) {
+  //   let startTime = null;
   
-    //get the current timestamp and assign it to the currentTime variable
-    let currentTime = Date.now();
+  //   //get the current timestamp and assign it to the currentTime variable
+  //   let currentTime = Date.now();
   
-    //pass the current timestamp to the step function
-    const step = (currentTime) => {
-      //if the start time is null, assign the current time to startTime
-      if (!startTime) {
-        startTime = currentTime;
-      }
+  //   //pass the current timestamp to the step function
+  //   const step = (currentTime) => {
+  //     //if the start time is null, assign the current time to startTime
+  //     if (!startTime) {
+  //       startTime = currentTime;
+  //     }
   
-      //calculate the value to be used in calculating the number to be displayed
-      const progress = Math.min((currentTime - startTime) / duration, 1);
+  //     //calculate the value to be used in calculating the number to be displayed
+  //     const progress = Math.min((currentTime - startTime) / duration, 1);
   
-      //calculate what to be displayed using the value gotten above
-      obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+  //     //calculate what to be displayed using the value gotten above
+  //     obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
   
-      //checking to make sure the counter does not exceed the last value (lastVal)
-      if (progress < 1) {
-        window.requestAnimationFrame(step);
-      } else {
-        window.cancelAnimationFrame(window.requestAnimationFrame(step));
-      }
-    };
-    //start animating
-    window.requestAnimationFrame(step);
-  }
-  let text1 = document.getElementById("year");
-  let text2 = document.getElementById("project");
-  const load = () => {
-    animate(text1, 0, 20, 7000);
-    animate(text2, 0, 100, 7000);
-  };
+  //     //checking to make sure the counter does not exceed the last value (lastVal)
+  //     if (progress < 1) {
+  //       window.requestAnimationFrame(step);
+  //     } else {
+  //       window.cancelAnimationFrame(window.requestAnimationFrame(step));
+  //     }
+  //   };
+  //   //start animating
+  //   window.requestAnimationFrame(step);
+  // }
+  // let text1 = document.getElementById("year");
+  // let text2 = document.getElementById("project");
+  // const load = () => {
+  //   animate(text1, 0, 20, 7000);
+  //   animate(text2, 0, 100, 7000);
+  // };
   
-  addEventListener("scroll", load());
+  // addEventListener("scroll", load());
   
   /* ======== MIXITUP FILTER PORTFOLIO ========== */
-  let mixerPortfolio = mixitup(".portfolio-container", {
-    selectors: {
-      target: ".portfolio-item",
-    },
-    animation: {
-      duration: 300,
-    },
-  });
+  // let mixerPortfolio = mixitup(".portfolio-container", {
+  //   selectors: {
+  //     target: ".portfolio-item",
+  //   },
+  //   animation: {
+  //     duration: 300,
+  //   },
+  // });
   
   /* ============= filter Active =============== */
   let tabfilter = document.querySelectorAll(".filter-portfolio button");
@@ -62,17 +62,6 @@ particlesJS.load("particles-js", "assets/particles.json", function () {
   
   tabfilter.forEach((l) => l.addEventListener("click", activefilter));
   
-  // Animation skills progress
-  let span = $(".progress-bar span").each(function () {
-    $(this).animate(
-      {
-        width: $(this).attr("data-progress") + "%",
-      },
-      2000
-    );
-  });
-  
-  $(".resume").scroll(span);
   
   /* ============ SCROLL SECTIONS ACTIVE LINK ============= */
   let sections = document.querySelectorAll("section[id]");
@@ -125,34 +114,8 @@ particlesJS.load("particles-js", "assets/particles.json", function () {
   }
   
   window.addEventListener("scroll", scrollResume);
-  
-  /* ============ Active card testimonial ============= */
-  let cardsTest = document.querySelectorAll(".testimonial .card");
-  
-  function activeCard() {
-    cardsTest.forEach((card) => card.classList.remove("active-card"));
-    this.classList.add("active-card");
-  }
-  
-  cardsTest.forEach((card) => card.addEventListener("click", activeCard));
-  
-  /* ============ Active card testimonial ============= */
-  let dataId = cardsTest.forEach((card) => card.getAttribute("data-id"));
-  let texts = document.querySelectorAll(".testimonial .text");
-  let textId = texts.forEach((text) => text.getAttribute("id"));
-  
-  function textActive() {
-    texts.forEach((text) => text.classList.remove("active-text"));
-    let current = this.getAttribute("data-id");
-    document
-      .querySelector(".testimonial .text[id*=" + current + "]")
-      .classList.add("active-text");
-  }
-  
-  cardsTest.forEach((card) => card.addEventListener("click", textActive));
-  
-  
-  // Typed.js ==========
+    
+  //  ============== Typed.js ==========
   var typed = new Typed("#typed", {
     strings: [ "Full Stack Developer", "Web Developer"],
     typeSpeed: 70,
@@ -167,4 +130,4 @@ particlesJS.load("particles-js", "assets/particles.json", function () {
   
   menuBar.addEventListener('click', () => {
     navMenu.classList.toggle('menu-active');
-  })
+  });
